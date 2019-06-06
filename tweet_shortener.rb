2259d664a237e0748c5ee231a6b287d8f63f7c0a
Nathan Rathe <nprathe10@gmail.com>
2019-06-06 22:34:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,27 @@
 # Write your code here.
 def dictionary
-  words = {"hello" => "hi", }
+  words = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
+  }
+end
 
-
+def word_substituter(tweet)
+  tweet.split(" ")
+  tweet.each_with_index do |word, index|
+    dictionary.keys.each do |key|
+      if key == word
+        tweet[index] = dictionary[key]
+      end
+    end
+  end
+  tweet.join(" ")
 end
